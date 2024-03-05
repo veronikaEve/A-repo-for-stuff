0cd4eb0ea722814c5f35611eb59870d7753b1f76
veronika <veronika.eve@sky.uk>
2024-03-05 10:59:08 +0000

    init vite

---
+++ app-viewer/src/main.ts
@@ -0,0 +1,10 @@
+import "./style.css";
+
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+  <div>
+    <h1>New Redirector POC</h1>
+    <p class="read-the-docs">
+      This is for the iframe
+    </p>
+  </div>
+`;
